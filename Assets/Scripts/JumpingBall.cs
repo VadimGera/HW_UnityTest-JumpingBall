190b10e99ebf12fd44fddc10bdad14c25b6d054e
VadimGera <vadimnvidia@gmail.com>
2023-06-08 22:31:06 +0300

    работает прыжок и консоль

---
+++ Assets/Scripts/JumpingBall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float initialHeight;
+    private bool isJumping;
+    private float jumpStartTime;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initialHeight = transform.position.y;
+        isJumping = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            Jump();
+        }
+
+        if (isJumping && Time.time - jumpStartTime >= 0.5f)
+        {
+            isJumping = false;
+            float currentHeight = transform.position.y;
+            float jumpHeight = currentHeight - initialHeight;
+            Debug.Log("Jump height: " + jumpHeight);
+        }
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        isJumping = true;
+        jumpStartTime = Time.time;
+    }
+}
+
